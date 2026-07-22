30e1e61ea64fa9b59194fccc086d4bd3cba44914
Anchal Yadav <anchalyadav0649@gmail.com>
2026-07-22 21:47:19 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.62%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       
+        if (x < 0) {
+        return false;
+    }
+
+    int original = x;
+    int rev = 0;
+
+    while (x != 0) {
+        int rem = x % 10;
+        
+        if (rev > (INT_MAX - rem) / 10) {
+         
+            return false;
+        }
+        rev = rev * 10 + rem;
+        x = x / 10;
+    }
+
+    return original == rev;
+}
+        
+        
+    
+};
